6ed4626f11ad72c7b4d6da8210512922a031ebf6
Nicholas Stamoulis <135749660+stamos11@users.noreply.github.com>
2024-06-26 21:05:12 +0300

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by stamoulis nikolaos on 26/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
